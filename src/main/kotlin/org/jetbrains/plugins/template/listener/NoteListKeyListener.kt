b6998f497b91b1e964e9244fcd5af747002ada42
stanley <stanleygomesdasilva@gmail.com>
2026-02-11 14:58:32 -0300

    feat: press enter key to open note

--- src/main/kotlin/org/jetbrains/plugins/template/listener/NoteListKeyListener.kt
+++ src/main/kotlin/org/jetbrains/plugins/template/listener/NoteListKeyListener.kt
@@ -4,10 +4,12 @@ import com.intellij.openapi.project.Project
 import org.jetbrains.plugins.template.dto.Note
 import org.jetbrains.plugins.template.service.DeleteNoteService
 import org.jetbrains.plugins.template.service.FavoriteNoteService
+import org.jetbrains.plugins.template.service.OpenNoteService
 import org.jetbrains.plugins.template.service.RenameNoteService
 import java.awt.event.KeyAdapter
 import java.awt.event.KeyEvent
 import java.awt.event.KeyEvent.VK_DELETE
+import java.awt.event.KeyEvent.VK_ENTER
 import java.awt.event.KeyEvent.VK_F
 import java.awt.event.KeyEvent.VK_F2
 
@@ -17,6 +19,12 @@ class NoteListKeyListener(
 ) : KeyAdapter() {
 
     override fun keyPressed(e: KeyEvent) {
+        if (e.keyCode == VK_ENTER) {
+            val selectedNote = getSelectedValue() ?: return
+            OpenNoteService()
+                .open(project, selectedNote)
+        }
+
         if (e.keyCode == VK_DELETE) {
             val selectedNote = getSelectedValue() ?: return
             DeleteNoteService()
