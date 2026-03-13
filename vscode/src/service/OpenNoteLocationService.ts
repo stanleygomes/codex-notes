50e6050dd2e76260ab038ca951853b948291dcd4
stanley <stanleygomesdasilva@gmail.com>
2026-03-13 14:48:31 -0300

    fix: open note folder correctly

--- vscode/src/service/OpenNoteLocationService.ts
+++ vscode/src/service/OpenNoteLocationService.ts
@@ -1,10 +1,8 @@
 import * as vscode from 'vscode';
 import { Note } from '../dto/Note';
-import { FileHelper } from '../helper/FileHelper';
 
 export class OpenNoteLocationService {
   openLocation(note: Note): void {
-    const dir = FileHelper.getParentPath(note.filePath);
-    vscode.commands.executeCommand('revealFileInOS', vscode.Uri.file(dir));
+    vscode.commands.executeCommand('revealFileInOS', vscode.Uri.file(note.filePath));
   }
 }
